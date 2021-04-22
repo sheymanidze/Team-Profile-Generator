a142e8ffcd046df6ddfaa4989845d87902aaa1ef
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-04-22 11:38:03 -0400

    questions for Employee added

--- index.js
+++ index.js
@@ -1,49 +1,63 @@
-// const inquirer = require('inquirer');
-// const fs = require('fs');
-// const util = require('util');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// const writeFileAsync = util.promisify(fs.writeFile);
+//constructor function
+const Employee = require('.lib/Employee.js');
+const Engineer = require('.lib/Engineer.js');
+const Intern = require('.lib/Intern.js');
+const Manager = require('.lib/Manager.js');
 
-// const promptUser = () => {
-//   return inquirer.prompt([
-//     {
-//       type: 'input',
-//       name: 'name',
-//       message: 'What is your name?',
-//     },
-//     {
-//       type: 'input',
-//       name: 'location',
-//       message: 'Where are you from?',
-//     },
-//     {
-//       type: 'input',
-//       name: 'hobby',
-//       message: 'What is your favorite hobby?',
-//     },
-//     {
-//       type: 'input',
-//       name: 'food',
-//       message: 'What is your favorite food?',
-//     },
-//     {
-//       type: 'input',
-//       name: 'github',
-//       message: 'Enter your GitHub Username',
-//     },
-//     {
-//       type: 'input',
-//       name: 'linkedin',
-//       message: 'Enter your LinkedIn URL.',
-//     },
-//   ]);
-// };
+//generate templates
+const generateTemplates = require('./src/template.js')
+const writeHTML = require('./generate.js')
 
-// const init = () => {
-//   promptUser()
-//     .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
-//     .then(() => console.log('Successfully wrote to index.html'))
-//     .catch((err) => console.error(err));
-// };
+const team = [];
 
-// init();
+function getInfo() {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: "Please write Employee's Name",
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter Employee's Name")
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: "Please provide Employee's ID",
+      validate: idInput => {
+        if (idInput) {
+          return true;
+        } else {
+          console.log("Please ener Employee's ID")
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "Please enter Employee's email address",
+      validate: emailInput => {
+        if (emailInput) {
+          return true;
+        } else {
+          console.log("Please enter Employee's email address")
+        }
+      }
+    },
+    {
+      type: 'list',
+      name: 'role',
+      message: "Please choose Employee's Role",
+      choices: ['Employee', 'Engineer', 'Intern', 'Manager']
+    },
+  ]);
+};
