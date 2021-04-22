038d916d7b1e553ea1fe303ef2d6e2f44e66e74d
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-04-22 11:02:21 -0400

    structure created

---
+++ index.js
@@ -0,0 +1,49 @@
+// const inquirer = require('inquirer');
+// const fs = require('fs');
+// const util = require('util');
+
+// const writeFileAsync = util.promisify(fs.writeFile);
+
+// const promptUser = () => {
+//   return inquirer.prompt([
+//     {
+//       type: 'input',
+//       name: 'name',
+//       message: 'What is your name?',
+//     },
+//     {
+//       type: 'input',
+//       name: 'location',
+//       message: 'Where are you from?',
+//     },
+//     {
+//       type: 'input',
+//       name: 'hobby',
+//       message: 'What is your favorite hobby?',
+//     },
+//     {
+//       type: 'input',
+//       name: 'food',
+//       message: 'What is your favorite food?',
+//     },
+//     {
+//       type: 'input',
+//       name: 'github',
+//       message: 'Enter your GitHub Username',
+//     },
+//     {
+//       type: 'input',
+//       name: 'linkedin',
+//       message: 'Enter your LinkedIn URL.',
+//     },
+//   ]);
+// };
+
+// const init = () => {
+//   promptUser()
+//     .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
+//     .then(() => console.log('Successfully wrote to index.html'))
+//     .catch((err) => console.error(err));
+// };
+
+// init();
